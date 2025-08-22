dd7688b81c1e8be6df1ec1b68e44a09ee1cc71b8
Skyler Morris <80974033+skyyMo@users.noreply.github.com>
2025-08-22 09:17:09 -0500

    Added Export

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+export default App;import React, { useState, useEffect } from 'react';
 
 function App() {
   const [transcripts, setTranscripts] = useState([]);
